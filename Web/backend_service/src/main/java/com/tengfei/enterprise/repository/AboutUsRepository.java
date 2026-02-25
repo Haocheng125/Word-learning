f9bd7810441c88f27a22db63c92ce78508ba529f
Haocheng Tang <Haocheng.Tang23@student.xjtlu.edu.cn>
2026-02-25 16:22:09 +0800

    Update project structure and fix 404 error

--- Web/backend_service/src/main/java/com/tengfei/enterprise/repository/AboutUsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tengfei.enterprise.repository;
-
-import com.tengfei.enterprise.entity.AboutUs;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AboutUsRepository extends JpaRepository<AboutUs, Long> {
-}
